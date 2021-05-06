5b9b1b064f547c2a01ced2a947fa5cb13dd6162f
InYuusha <asuse975@gmail.com>
2021-05-06 18:42:19 +0530

    networks

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-//
+mongoose.connect("mongodb://inyuusha:mypass@mongo/?authSource=admin")
+.then(()=>console.log("Server connected to database"))
+.catch((err)=>console.log(e))
+
 //routes
 app.get('/',(req,res)=>{
-    res.send("<h2>Hello World</h2>")
+    res.send("<h2>Hello World111</h2>")
 })
 
 const port = process.env.PORT||3000;
